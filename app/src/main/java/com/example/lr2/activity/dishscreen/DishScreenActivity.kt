19a3cf444f71719c42768cb7c84830c8d4c5cf9b
Filimoncev <ilya.filimoncev@mail.ru>
2024-11-22 17:00:21 +0700

    Экран блюда, версия 2. В main activity была добавлена вторая кнопка, ведущая к экрану с блюдом, минуя экран с меню. Пытаюсь разобраться, как передать значения из класса в TextView.

--- app/src/main/java/com/example/lr2/activity/dishscreen/DishScreenActivity.kt
+++ app/src/main/java/com/example/lr2/activity/dishscreen/DishScreenActivity.kt
@@ -17,6 +17,21 @@ class DishScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dish_screen)
+
+            val dish1 = DishInfo()
+            dish1.dishName = "Цезарь"
+            dish1.dishImagePath = "toDo"
+            dish1.weight = 300
+            dish1.ingredients = "Лаваш, филе цыплёнка, соус на выбор, огурец, томаты, сыр моцарелла, капуста, картофель фри."
+            dish1.sauceImagePath = "toDo"
+
+            val dish2 = DishInfo()
+            dish2.dishName = "Гавайская"
+            dish2.dishImagePath = "toDo"
+            dish2.weight = 300
+            dish2.ingredients = "Лаваш, филе цыплёнка, соус на выбор, капуста, ананас, сыр моцарелла."
+            dish2.sauceImagePath = "toDo"
+
     }
 
     fun backFromDish(view: View){
@@ -29,10 +44,34 @@ class DishScreenActivity : AppCompatActivity() {
         startActivity(cartFromDishIntent)
     }
 
-}
 
-class dishSauceAdapter(private val sauce: List<String>){
+    class DishInfo {
+        var dishImagePath: String? = null
+        var dishName: String? = null
+        var ingredients: String? = null
+        var weight: Int = 0
+        var sauceImagePath: String? = null
+    }
+
+   /* fun  dishesList(){
+        val dish1 = DishInfo()
+        dish1.dishName = "Цезарь"
+        dish1.dishImagePath = "toDo"
+        dish1.weight = 300
+        dish1.ingredients = "Лаваш, филе цыплёнка, соус на выбор, огурец, томаты, сыр моцарелла, капуста, картофель фри."
+        dish1.sauceImagePath = "toDo"
+
+        val dish2 = DishInfo()
+        dish2.dishName = "Гавайская"
+        dish2.dishImagePath = "toDo"
+        dish2.weight = 300
+        dish2.ingredients = "Лаваш, филе цыплёнка, соус на выбор, капуста, ананас, сыр моцарелла."
+        dish2.sauceImagePath = "toDo"
+    } */
+}
+/*class dishSauceAdapter(private val sauce: List<String>){
     class dishSauceViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val dishImageView: View = itemView.findViewById(R.id.sauceImage)
     }
-}
+
+}*/
